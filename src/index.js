c1e59b34f7cce7a9b0d5d2a7e864ee62dc272a22
Marat Davletgareev <davletgareev.mn@gmail.com>
2021-08-16 14:28:15 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './css/styles.css';
 
 import './js/app.js';
+import './js/domain.js';
+import './js/game.js';
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     port: 8000,
     historyApiFallback: true,
     hot: true,
-},
+  },
   module: {
     rules: [
       {
